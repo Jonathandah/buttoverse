e1e587c3de62f1d30e6df1ce57fb57031b03c13b
Jonathan Meeprong Dahnberg <jonathan.dahnberg1@gmail.com>
2024-03-16 13:40:13 +0100

    Add Tailwind CSS configuration and styles

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { cn } from '$lib/utils/cn.js';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLButtonAttributes {}
+
+	const { class: klass, ...props }: $$Props = $$props;
+</script>
+
+<button class={cn('h-10 px-4 py-2', klass)} {...props}>
+	<slot />
+</button>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="flex flex-col h-dvh  bg-gray-900 relative">
+	<div aria-hidden class=" inset-1/2 absolute inset-x-0 w-16 h-16 rounded-full  bg-gradient-to-b from-pink-500 to-purple-600 blur-3xl mx-auto  opacity-75"/>
+	<slot />
+</main>
