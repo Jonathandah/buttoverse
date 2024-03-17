5ab2f0611e42473a4de1afb15222895420f981e2
Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>
2024-03-17 10:20:47 +0100

    npx run prettier write

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,10 @@
 	import '../app.css';
 </script>
 
-<main class="relative flex flex-col h-dvh">
-	<div aria-hidden class="absolute inset-x-0 w-16 h-16 mx-auto rounded-full opacity-75 inset-1/3 bg-gradient-to-b from-pink-500 to-purple-600 blur-3xl"/>
+<main class="relative flex h-dvh flex-col">
+	<div
+		aria-hidden
+		class="absolute inset-1/3 inset-x-0 mx-auto h-16 w-16 rounded-full bg-gradient-to-b from-pink-500 to-purple-600 opacity-75 blur-3xl"
+	/>
 	<slot />
 </main>
