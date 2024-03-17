16f2b2b497b240b20457f20808344609d572cbc7
Jonathan Meeprong Dahnberg <jonathan.dahnberg@skymill.io>
2024-03-17 10:18:39 +0100

    Refactor layout styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<main class="flex flex-col h-dvh  bg-gray-900 relative">
-	<div aria-hidden class=" inset-1/2 absolute inset-x-0 w-16 h-16 rounded-full  bg-gradient-to-b from-pink-500 to-purple-600 blur-3xl mx-auto  opacity-75"/>
+<main class="relative flex flex-col h-dvh">
+	<div aria-hidden class="absolute inset-x-0 w-16 h-16 mx-auto rounded-full opacity-75 inset-1/3 bg-gradient-to-b from-pink-500 to-purple-600 blur-3xl"/>
 	<slot />
 </main>
